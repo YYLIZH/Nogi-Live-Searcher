56a8abca1ee42469c6d90eed1a1d0f137bb12eef
ECHO <ryne91009@gmail.com>
2021-01-06 17:06:59 +0800

    fixed the channel

--- popup.js
+++ popup.js
@@ -46,16 +46,24 @@ async function fromTimeGetUrl(timeTicker) {
                 if (anchors[i].textContent == '클릭') {
                     if (String(anchors[i].getAttribute('href')).slice(-8) !== "chatroom") {
                         success = true
-                        let str = String(anchors[i]).split('/')
-                        let twitch = 'https://www.twitch.tv/' + str[str.length - 1]
-                        let newAnchor = document.createElement('a')
-                        let br = document.createElement('br')
-                        newAnchor.setAttribute('href', twitch)
-                        newAnchor.setAttribute('target', "_blank")
-                        newAnchor.textContent = livePostTitle
-                        display.appendChild(newAnchor)
-                        display.appendChild(br)
-                        display.appendChild(br)
+                        fetch(anchors[i].getAttribute('href'))
+                            .then(response => {
+                                return response.text()
+                            })
+                            .then(res => {
+                                let parser = new DOMParser()
+                                let doc = parser.parseFromString(res, 'text/html')
+                                let src = doc.querySelector('iframe').getAttribute('src')
+                                let twitch = 'https://www.twitch.tv/' + src.slice(src.indexOf("channel") + 8, src.indexOf("&"))
+                                let newAnchor = document.createElement('a')
+                                let br = document.createElement('br')
+                                newAnchor.setAttribute('href', twitch)
+                                newAnchor.setAttribute('target', "_blank")
+                                newAnchor.textContent = livePostTitle
+                                display.appendChild(newAnchor)
+                                display.appendChild(br)
+                                display.appendChild(br)
+                            })
                     }
                 }
             }
@@ -73,3 +81,4 @@ async function fromTimeGetUrl(timeTicker) {
 
 //https://www.twitch.tv/dbsktkaxh
 //https://translate.google.com.tw/?hl=zh-TW&sl=ko&tl=ja&text=%EC%9D%BC%EB%B3%B8%20%EC%95%84%EC%82%AC%ED%9E%88TV%20LIVE%20%EB%86%80%EB%9E%98%ED%82%A4%EB%8A%94%20%EC%A0%80%ED%83%9D%20TRICK%20HOUSE%20%5B%ED%83%80%EB%AC%B4%EB%9D%BC%20%EB%A7%88%EC%9C%A0%5D%20(23%3A15~&op=translate
+//https://player.twitch.tv/?channel=potsurito&parent=poooo.ml
